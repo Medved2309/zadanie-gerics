f502ad3fa5096e9a8b487cbb1bbb79c97e4be764
Andrey Dmitrievich <medved2309@gmail.com>
2022-10-17 16:38:18 +0500

    reshenie

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,36 @@
 import box.MagicBox;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        numberMagicBox();
+        stringMagicBox();
+    }
 
-        MagicBox<Integer> magicBox = new MagicBox<>();
-        //System.out.println(magicBox.add(22));
-        //System.out.println(magicBox.add(10));
-        //System.out.println(magicBox.add(40));
-        //System.out.println(magicBox.add(300));
-        //System.out.println(magicBox.add(25));
-
-        while (true) {
-            System.out.println("Введите число или end");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            if ("end".equals(input)) {
-                System.out.println("Заполнение коробке окончено");
-                break;
-
-            } else {
-                String[] parts = input.split(" ");
-               Integer Ba = Integer.parseInt(parts[0]) - 1;
-                System.out.println(magicBox.add(Ba));
-
-            }
-        }
-
+    private static void numberMagicBox() {
+        System.out.println("Магическая коробка с цифрами:");
+        MagicBox<Integer> magicBox = new MagicBox<>(5);
+        System.out.println(magicBox.add(50));
+        System.out.println(magicBox.add(7));
+        System.out.println(magicBox.add(10));
+        System.out.println(magicBox.add(100));
+        System.out.println(magicBox.add(70));
+        System.out.println(magicBox.add(70));
+        Integer pick = magicBox.pick();
+        System.out.println("Случайная цифра из коробки: " + pick);
 
     }
 
+    private static void stringMagicBox() {
+        System.out.println("Магическая коробка со словами");
+        MagicBox<String> magicBox = new MagicBox<>(4);
+        System.out.println(magicBox.add("Pety"));
+        System.out.println(magicBox.add("Птички"));
+        System.out.println(magicBox.add("Медведи"));
+        System.out.println(magicBox.add("Бусы"));
+        System.out.println(magicBox.add("Медведи"));
+        String pick = magicBox.pick();
+        System.out.println("Случайное слово из коробки: " + pick);
+
+    }
 
 }
--- src/box/MagicBox.java
+++ src/box/MagicBox.java
@@ -1,22 +1,39 @@
 package box;
 
+import java.util.Random;
 
 public class MagicBox<T> {
+    Random random = new Random();
 
-    protected int SIZE = 5;
+    private T[] items;
 
-    T[] items = (T[]) new Object[SIZE];
+    public MagicBox(int SIZE) {
+        this.items = (T[]) new Object[SIZE];
+    }
 
-    public boolean add(T b) { // Заполнение коробки
-        for (int i = 0; i < SIZE; i++) {
+
+    public boolean add(T item) { // Заполнение коробки
+        for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
-                items[i] = b;
+                items[i] = item;
+                System.out.println("Добавили " + item);
                 return true;
             }
         }
+        System.out.println("Коробка заполнена");
         return false;
     }
 
+    public T pick() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException(String.format("Коробка не заполна, необходимо добавить в коробку %d элементов", items.length - i));
+            }
+        }
+        int randomInt = random.nextInt(items.length);
+        return items[randomInt];
+
+    }
 }
 
 
