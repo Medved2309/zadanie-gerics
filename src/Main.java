58922192edd686eea6485b0fe7119f879035a139
Andrey Dmitrievich <medved2309@gmail.com>
2022-10-17 15:21:43 +0500

    Box var1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import box.MagicBox;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         MagicBox<Integer> magicBox = new MagicBox<>();
+        Random random = new Random();
+        int randomInt = random.nextInt(); // джава подберёт случайное число от 0 до ЧИСЛО невключительно
         //System.out.println(magicBox.add(22));
         //System.out.println(magicBox.add(10));
         //System.out.println(magicBox.add(40));
@@ -17,13 +20,23 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             if ("end".equals(input)) {
-                System.out.println("Заполнение коробке окончено");
-                break;
+                if (magicBox.pick() == 0) {
+                    System.out.println("Коробка заполнена полностью");
+                  //  System.out.println("Случайное число из коробки " + );
+                    break;
+                } else {
+                    System.out.println("RuntimeException коробка не заполнена осталось ввести ячеек " + magicBox.pick());
+                }
 
             } else {
                 String[] parts = input.split(" ");
-               Integer Ba = Integer.parseInt(parts[0]) - 1;
-                System.out.println(magicBox.add(Ba));
+                Integer item = Integer.parseInt(parts[0]);
+                if (magicBox.add(item)){
+                    System.out.println("RuntimeException коробка не заполнена полностью осталось ввести ячеек " + magicBox.pick());
+                } else {
+                    System.out.println("Коробка заполнена");
+                  //  System.out.println("Случайное число из коробки " + );
+                }
 
             }
         }
--- src/box/MagicBox.java
+++ src/box/MagicBox.java
@@ -7,14 +7,28 @@ public class MagicBox<T> {
 
     T[] items = (T[]) new Object[SIZE];
 
-    public boolean add(T b) { // Заполнение коробки
-        for (int i = 0; i < SIZE; i++) {
+    public boolean add(T item) { // Заполнение коробки
+        for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
-                items[i] = b;
+                items[i] = item;
+                System.out.println("Добавили " + item);
                 return true;
             }
         }
         return false;
+    }
+
+    public int pick() { // проверка на заполнение коробки
+        int difference = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                difference = items.length - i +1;
+                break;
+
+
+            }
+        }
+        return difference;
     }
 
 }
